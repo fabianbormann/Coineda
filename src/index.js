7c6461103847ad3a3638c52d4a0b9af2d567be04
fabianbormann <bormann.fabian@gmail.com>
2022-04-16 00:08:28 +0200

    enable service worker for app.coineda.io

--- src/index.js
+++ src/index.js
@@ -4,5 +4,12 @@ import App from './App';
 import './i18n';
 import 'antd/dist/antd.less';
 import './index.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+if (process.env.REACT_APP_TARGET === 'single-page-application') {
+  serviceWorkerRegistration.register();
+} else {
+  serviceWorkerRegistration.unregister();
+}
