8e7f8df6eb3f2ac79bf75096e1b20fbc140b4677
Fabian Bormann <fabian.bormann@cardanofoundation.org>
2023-01-24 21:35:24 +0100

    fix: remove jss

--- src/pages/Issues.tsx
+++ src/pages/Issues.tsx
@@ -1,30 +1,12 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { createUseStyles } from 'react-jss';
-
-const useStyles = createUseStyles({
-  page: {
-    padding: 16,
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  headline: {
-    marginTop: 0,
-    marginBottom: 12,
-    fontWeight: 500,
-    fontSize: '1.1rem',
-    color: '#2F4858',
-  },
-});
 
 const Issues = () => {
   const { t } = useTranslation();
-  const classes = useStyles();
 
   return (
-    <div className={classes.page}>
-      <p className={classes.headline}>{t('Issues') as string}</p>
+    <div>
+      <p>{t('Issues') as string}</p>
       <div>No issues detected</div>
     </div>
   );
